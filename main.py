d61dc371d4351a8e3000996d6449d18ae986f143
quabwww <josuebuenovargas@gmail.com>
2024-02-07 15:34:35 -0500

    ok

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ def on_route():
     return "200 ON"
 
 
+@app.get("/example/")
+def example():
+    return "Example"
+
+
 if __name__ == "__main__":
     uvicorn.run(app=app, host="0.0.0.0", port=8000)
